10177b78d0be0957159424c62182276a03c8789f
Nícholas Eras <48137930+Nicholamais@users.noreply.github.com>
2021-02-08 20:50:21 -0300

    vf1

--- Simulador.py
+++ Simulador.py
@@ -9,12 +9,9 @@ from matplotlib.figure import Figure
 from matplotlib.widgets import Slider
 import matplotlib.ticker as plticker
 
-
 matplotlib.use("TkAgg")
 
-
-arquivo = pd.read_excel(r"C:\Users\nicoe\PycharmProjects\CodigoSimulacao\e1.xlsx")
-#print(arquivo)
+arquivo = pd.read_excel(r"C:\Users\nicoe\PycharmProjects\CodigoSimulacao\dados.xlsx")
 
 
 def Refetividade(t):
@@ -40,16 +37,13 @@ def Refetividade(t):
     for i in range(0, len(data[0])):
         data[0][i] = data[0][i] / 1000000000
 
-    return [data[0],rl]
+    return [data[0], rl]
 
 
 janela = Tk()
 janela.state("zoomed")
 janela.title("Simulador")
-
-
-
-
+janela.minsize(800,600)
 
 fi = Figure(figsize=(5, 4), dpi=100)
 fi.subplots_adjust(bottom=0.35)
@@ -57,28 +51,27 @@ fi.subplots_adjust(bottom=0.35)
 canvas = FigureCanvasTkAgg(fi, master=janela)
 canvas.draw()
 canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-NavigationToolbar2Tk(canvas,janela)
+NavigationToolbar2Tk(canvas, janela)
 canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 entryEsp = Entry(janela)
-entryEsp.pack(side="left",expand=True,fill='x')
-entryEsp.insert(0,'0.004')
+entryEsp.pack(side="left", expand=True, fill='x')
+entryEsp.insert(0, '0.004')
+
 
 def atualiza_plot():
     pos = float(entryEsp.get())
     sld_var.set_val(pos)
     update(sld_var)
 
-btn = Button(janela,text = "OK", command = atualiza_plot)
-btn.pack(side="right",expand=True,fill='x')
 
+btn = Button(janela, text="OK", command=atualiza_plot)
+btn.pack(side="right", expand=True, fill='x')
 
-
-#plt.axis([8,13,-30,0])
 a = fi.add_subplot(111)
 dadosxy = Refetividade(.004)
-line, = a.plot(dadosxy[0],dadosxy[1])
-a.set_ylim(-30,0)
+line, = a.plot(dadosxy[0], dadosxy[1])
+a.set_ylim(-30, 0)
 a.grid()
 
 tick = plticker.MultipleLocator(base=0.5)
@@ -87,25 +80,19 @@ a.set_title('Simulation')
 a.set_ylabel('Reflection Loss (dB)')
 a.set_xlabel('Frequency (GHz)')
 
+ax_var = fi.add_axes([0.12, 0.1, 0.78, 0.03])
+sld_var = Slider(ax_var, "Thickness", .002, .009, valinit=.004)
 
-ax_var = fi.add_axes([0.12,0.1,0.78,0.03])
-sld_var = Slider(ax_var,"Espessura",.002,.009,valinit=.004)
+
 def update(val):
     pos = sld_var.val
-    entryEsp.delete(0,'end')
-    entryEsp.insert(0,("%.3f"%pos))
+    entryEsp.delete(0, 'end')
+    entryEsp.insert(0, ("%.3f" % pos))
     dadosxy = Refetividade(pos)
-    line.set_data(dadosxy[0],dadosxy[1])
+    line.set_data(dadosxy[0], dadosxy[1])
     fi.canvas.draw_idle()
+
 
 sld_var.on_changed(update)
 
-
-
-
-
-
-
-
 tk.mainloop()
-
--- teste.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-
-if __name__ == '__main__':
-
-   root = tk.Tk()
-   run = Passwordchecker(root)
-   root.mainloop()
